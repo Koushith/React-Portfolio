c422ddae869f02d42d86f0e3123947c018bd17a8
koushith <koushith97@gmail.com>
2020-04-30 23:08:25 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './style.css';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
+import Footer from './components/Footer';
+import Copy from './components/CopyRight';
+import About from './components/About';
+import Contact from './components/Contact';
+// import Portfolio from './components/Portfolio';
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Hero />
+      {/* <Portfolio /> */}
+      <About />
+      <Contact />
+      <Footer />
+      <Copy />
     </div>
   );
 };
